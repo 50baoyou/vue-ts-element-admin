f6ee6107a9b7fa79bdaea47ac8eaa237820a7b0e
guizidongle <517900455@qq.com>
2024-01-15 12:03:49 +0800

    chore: :hammer: 修改部分配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,15 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv, ConfigEnv, UserConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import { compression } from 'vite-plugin-compression2'
 import { createHtmlPlugin } from 'vite-plugin-html'
-
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
-
 import svgLoader from 'vite-svg-loader'
-
 import UnoCSS from 'unocss/vite'
 
 function path(url: string): string {
@@ -26,23 +24,17 @@ export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
   // 根据当前工作目录中的 `mode` 加载 .env 文件
   const env = loadEnv(mode, process.cwd())
 
-  const appConfig = {
-    resolve: {
-      alias: {
-        '@': path('./src')
-      }
-    },
-    css: {
-      // CSS 预处理器
-      preprocessorOptions: {
-        // 定义全局 SCSS 变量
-        scss: {
-          additionalData: `@use "@/styles/variables.scss" as *;`
-        }
-      }
+  return {
+    // 删除所以控制台和调试语句
+    esbuild: {
+      drop: ['console', 'debugger']
     },
     plugins: [
       vue(),
+      compression({
+        include: ['js', 'css'],
+        threshold: 500
+      }),
       createHtmlPlugin({
         // 压缩 html
         minify: true,
@@ -55,6 +47,7 @@ export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
         // 如果 vite 版本高于 5.0.0-beta.13，可以将 viteNext 设置为 true 来进行兼容
         viteNext: true
       }),
+      ViteImageOptimizer(),
       svgLoader(),
       UnoCSS(),
       Icons({
@@ -90,91 +83,69 @@ export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
         // 指定自动导入组件TS类型声明文件路径
         dts: path('./src/types/components.d.ts')
       })
-    ]
-  }
-
-  const envConfig = (command: 'build' | 'serve') => {
-    if (command === 'serve') {
-      // dev 独有配置
-      return {
-        server: {
-          // 允许IP访问
-          host: true,
-          // 应用端口 (默认:5173)
-          port: env.VITE_APP_PORT,
-          // 运行是否自动打开浏览器
-          open: true,
-          proxy: {
-            // 需要代理的接口：env.VITE_APP_API
-            [env.VITE_APP_API]: {
-              // 允许跨域
-              changeOrigin: true,
-              // 目标接口
-              target: '填写接口域名',
-              // 重写请求路径
-              rewrite: (path: string) => path.replace(new RegExp('^' + env.VITE_APP_API), '')
-            }
-          }
+    ],
+    resolve: {
+      alias: {
+        '@': path('./src')
+      }
+    },
+    css: {
+      // CSS 预处理器
+      preprocessorOptions: {
+        // 定义全局 SCSS 变量
+        scss: {
+          additionalData: `@use "@/styles/variables.scss" as *;`
         }
       }
-    }
-
-    if (command === 'build') {
-      // build 独有配置
-      return {
-        build: {
-          // 消除打包大小超过500kb警告
-          chunkSizeWarningLimit: 2000,
-          // Vite 2.6.x 以上需要配置 minify: "terser", terserOptions 才能生效
-          minify: 'terser',
-          terserOptions: {
-            compress: {
-              // 防止 Infinity 被压缩成 1/0，这可能会导致 Chrome 上的性能问题
-              keep_infinity: true,
-              // 在生产环境中移除 console 语句，减少代码体积和避免在用户端显示日志信息
-              drop_console: true,
-              // 在生产环境中移除 debugger 语句，防止调试代码进入生产版本
-              drop_debugger: true
-            },
-            format: {
-              // 设置为 false 以删除代码中的注释
-              comments: false
+    },
+    server: {
+      // 允许IP访问
+      host: true,
+      // 应用端口 (默认:5173)
+      port: command === 'serve' ? Number(env.VITE_APP_PORT) : 8080,
+      // 运行是否自动打开浏览器
+      open: true,
+      proxy: {
+        // 需要代理的接口：env.VITE_APP_API
+        [env.VITE_APP_API]: {
+          // 允许跨域
+          changeOrigin: true,
+          // 目标接口
+          target: command === 'serve' ? `http://localhost:${env.VITE_APP_PORT}` : '填写接口域名',
+          // 重写请求路径
+          rewrite: (path: string) => path.replace(new RegExp('^' + env.VITE_APP_API), '')
+        }
+      }
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          // 拆分 chunk
+          manualChunks(id: string) {
+            if (id.includes('node_modules')) {
+              // 通过 split 获取 node_modules 包的名称
+              return id.split('node_modules/')[1].split('/')[0]
             }
           },
-          rollupOptions: {
-            output: {
-              // manualChunks: {
-              //   "vue-i18n": ["vue-i18n"],
-              // },
-              // 用于从入口点创建的块的打包输出格式 [name] 表示文件名，[hash] 表示该文件内容 hash 值
-              entryFileNames: 'js/[name].[hash].js',
-              // 用于命名代码拆分时创建的共享块的输出命名
-              chunkFileNames: 'js/[name].[hash].js',
-              // 用于输出静态资源的命名，[ext] 表示文件扩展名
-              assetFileNames: (assetInfo: any) => {
-                const info = assetInfo.name.split('.')
-                let extType = info[info.length - 1]
-                // console.log('文件信息', assetInfo.name)
-
-                if (/\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/i.test(assetInfo.name)) {
-                  extType = 'media'
-                } else if (/\.(png|jpe?g|gif|svg)(\?.*)?$/.test(assetInfo.name)) {
-                  extType = 'img'
-                } else if (/\.(woff2?|eot|ttf|otf)(\?.*)?$/i.test(assetInfo.name)) {
-                  extType = 'fonts'
-                }
-
-                return `${extType}/[name].[hash].[ext]`
-              }
+          // 用于从入口点创建的块的打包输出格式 [name] 表示文件名，[hash] 表示该文件内容 hash 值
+          entryFileNames: 'js/[name].[hash].js',
+          // 用于命名代码拆分时创建的共享块的输出命名
+          chunkFileNames: 'js/[name].[hash].js',
+          // 用于输出静态资源的命名，[ext] 表示文件扩展名
+          assetFileNames: (assetInfo: any) => {
+            const info = assetInfo.name.split('.')
+            let extType = info[info.length - 1]
+            if (/\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/i.test(assetInfo.name)) {
+              extType = 'media'
+            } else if (/\.(png|jpe?g|gif|svg)(\?.*)?$/.test(assetInfo.name)) {
+              extType = 'img'
+            } else if (/\.(woff2?|eot|ttf|otf)(\?.*)?$/i.test(assetInfo.name)) {
+              extType = 'fonts'
             }
+            return `${extType}/[name].[hash].[ext]`
           }
         }
       }
     }
   }
-
-  return {
-    ...appConfig,
-    ...envConfig(command)
-  } as UserConfig
 })
